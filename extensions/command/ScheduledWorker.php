f51a6fbde206318caadb0c3723a120cc5f2996a5
Phil <pnomolos@gmail.com>
2012-11-06 16:09:16 -0800

    Updating ScheduleWorker to support daemonized mode

--- extensions/command/ScheduledWorker.php
+++ extensions/command/ScheduledWorker.php
@@ -69,7 +69,38 @@ class ScheduledWorker extends \lithium\console\Command {
         $instance->logLevel = $this->loglevel;
 
         $this->out("Scheduled Worker is up listening at {$this->host}:{$this->port} every {$this->interval} seconds \n");
-        $instance->work( $this->interval );
+        
+        if (($pidfile = $this->pidfile) && $this->D) {
+          $pid = file_get_contents($pidfile);
+          if ($pid && !posix_kill($pid, SIGQUIT)) {
+            $this->out("Error: Unable to shutdown previous worker");
+            die;
+          }
+        }
+        
+        if ($this->D) {
+          $pid = pcntl_fork();
+          if (-1 == $pid) {
+            $this->out("Error: Unable to fork");
+            die;
+          } else if ($pid) {
+            // Shutdown the parent
+            $this->out("Successfully backgrounded job");
+            exit(0);
+          }
+          // We're in the child now
+          posix_setsid();
+          if ($pidfile = $this->pidfile) {
+            file_put_contents($pidfile, posix_getpid());
+          }
+          fclose(STDOUT); fclose(STDERR); fclose(STDIN);
+          ob_start(); // Capture all output
+          $instance->work($this->interval);
+          ob_end_flush();
+        } else {
+          $instance->work($this->interval);
+        }
+        
     }
 
 
