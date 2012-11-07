bf7e4b9220a889133a25c6bb4010599286aa1585
Phil <pnomolos@gmail.com>
2012-11-06 16:08:30 -0800

    Updating ResqueWorker to support daemonized mode

--- extensions/command/ResqueWorker.php
+++ extensions/command/ResqueWorker.php
@@ -66,7 +66,37 @@ class ResqueWorker extends \lithium\console\Command {
         $instance->logLevel = $this->loglevel;
 
         $this->out("Worker is up listening to queues '" . join(',', $this->queues) ."' at {$this->host}:{$this->port} \n");
-        $instance->work();
+        
+        if (($pidfile = $this->pidfile) && $this->D) {
+          $pid = file_get_contents($pidfile);
+          if ($pid && !posix_kill($pid, SIGQUIT)) {
+            $this->out("Error: Unable to shutdown previous worker");
+            die;
+          }
+        }
+        
+        if ($this->D) {
+          $pid = pcntl_fork();
+          if (-1 == $pid) {
+            $this->out("Error: Unable to fork");
+            die;
+          } else if ($pid) {
+            // Shutdown the parent
+            $this->out("Successfully backgrounded job");
+            exit(0);
+          }
+          // We're in the child now
+          posix_setsid();
+          if ($pidfile = $this->pidfile) {
+            file_put_contents($pidfile, posix_getpid());
+          }
+          fclose(STDOUT); fclose(STDERR); fclose(STDIN);
+          ob_start(); // Capture all output
+          $instance->work();
+          ob_end_flush();
+        } else {
+          $instance->work();
+        }
     }
 
 
